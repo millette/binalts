62bcbb9ef5219572498930fd7476093e8bca4f29
Robin Millette <robin@millette.info>
2020-03-02 22:07:23 -0500

    Add server script.

---
+++ srv.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const fastify = require("fastify")({ logger: true })
+
+fastify.register(require("fastify-compress"))
+fastify.register(require("fastify-leveldb"), {
+  name: "tickers-copy-v5",
+  options: {
+    valueEncoding: "json",
+  },
+})
+
+fastify.register(require("fastify-static"), {
+  root: __dirname,
+})
+
+const symbols = [
+  "FUNBTC",
+  "CVCBTC",
+  "DASHBTC",
+  "DNTBTC",
+  "LTCBTC",
+  "ADABTC",
+  "ETHBTC",
+  "XMRBTC",
+  "DCRBTC",
+  "SALTBTC",
+  "REPBTC",
+]
+
+fastify.get("/stocks.csv", (request, reply) => {
+  reply.type("text/plain")
+  const ddd = ["date,symbol,price"]
+  fastify.level.createReadStream()
+    .on("data", ({ key, value }) => {
+      symbols.forEach((symbol) => {
+        const price = value[symbol]
+        if (price) ddd.push([key, symbol, price].join())
+      })
+    })
+    .on("end", () => {
+      reply.send(ddd.join("\n"))
+    })
+})
+
+fastify.get("/favicon.ico", (request, reply) => reply.code(404).send("not found"))
+fastify.get("/", (request, reply) => reply.sendFile("index.html"))
+
+// Run the server!
+fastify.listen(3000, function(err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`server listening on ${address}`)
+})
